c3a71ec9ea7d6ad7e53fcd2b92d54a3922a4a9c0
Ujval Joshi <joshiujval@gmail.com>
2014-05-20 12:49:12 +0530

    Changes in ips

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,6 +1,6 @@
 Refinery::PagesController.class_eval do
 	
-	before_filter :find_all_teammembers, :find_all_boas, :find_all_blocks, :find_all_industries, :find_all_spaces, :find_all_digitals, :find_all_brands, :find_all_selfs, :find_all_products, :find_all_startups
+	before_filter :find_all_teammembers, :find_all_boas, :find_all_blocks, :find_all_industries, :find_all_spaces, :find_all_digitals, :find_all_brands, :find_all_selfs, :find_all_products, :find_all_startups, :find_all_ips
 	
 	protected
 	
@@ -31,6 +31,10 @@ Refinery::PagesController.class_eval do
 		
 		def find_all_startups
 			@startups ||= ::Refinery::Startups::Startup.order('position ASC')
+		end
+		
+		def find_all_ips
+			@ips ||= ::Refinery::Ips::Ip.order('position ASC')
 		end
 		
 		
