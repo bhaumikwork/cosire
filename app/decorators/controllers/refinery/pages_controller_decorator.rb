1ae38eea63460eb5492b1c346337055820da34f7
Ujval Joshi <joshiujval@gmail.com>
2014-05-23 15:35:29 +0530

    4 projects in services page

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,9 +1,9 @@
 Refinery::PagesController.class_eval do
 	
-	before_filter :find_all_teammembers, :find_all_boas, :find_all_blocks, :find_all_industries, :find_all_spaces, :find_all_digitals, :find_all_brands, :find_all_selfs, :find_all_products, :find_all_startups, :find_all_ips, :find_all_achievements
+before_filter :find_all_teammembers, :find_all_boas, :find_all_blocks, :find_all_industries, :find_all_spaces, :find_all_digitals, :find_all_brands, :find_all_selfs, :find_all_products, :find_all_startups, :find_all_ips, :find_all_achievements, :find_all_serviceprojects, :find_all_servicestrategies, :find_all_servicebrands, :find_all_servicepackages, :find_all_serviceuis, :find_all_servicespaces
 	
 	protected
-	
+	   
 		def find_all_products
 			@products ||= ::Refinery::Projects::Project.order('position ASC')
 		end
@@ -28,19 +28,17 @@ Refinery::PagesController.class_eval do
 			@selfs ||= ::Refinery::Projects::Project.where('work_category LIKE ?', '%self%').order('position ASC')
 		end
 		
-		
 		def find_all_startups
 			@startups ||= ::Refinery::Startups::Startup.order('position ASC')
 		end
 		
 		def find_all_ips
 			@ips ||= ::Refinery::Ips::Ip.order('position ASC')
-		end
+		end		
 		
 		def find_all_achievements
 			@achievements ||= ::Refinery::Achievements::Achievement.order('position ASC')
 		end
-		
 		
 		def find_all_teammembers
 			@teammembers ||= ::Refinery::Teammembers::Teammember.order('position ASC')
@@ -53,6 +51,31 @@ Refinery::PagesController.class_eval do
 		def find_all_blocks
 			@blocks ||= ::Refinery::Blocks::Block.all 
 			@count = 0
+		end
+		
+		
+		def find_all_serviceprojects
+			@serviceprojects ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%product%').order('position ASC').limit 4
+		end
+		
+		def find_all_servicestrategies
+			@servicestrategies ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%strategy%').order('position ASC').limit 4
+		end
+		
+		def find_all_servicebrands
+			@servicebrands ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%brand%').order('position ASC').limit 4
+		end
+		
+		def find_all_servicepackages
+			@servicepackages ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%package%').order('position ASC').limit 4
+		end
+		
+		def find_all_serviceuis
+			@serviceuis ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%digital%').order('position ASC').limit 4
+		end
+		
+		def find_all_servicespaces
+			@servicespaces ||= ::Refinery::Projects::Project.where('service_category LIKE ?', '%space%').order('position ASC').limit 4
 		end
 		
 end
